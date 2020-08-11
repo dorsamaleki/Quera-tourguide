a21bc8b5a1decb1faefb3a75aa26946f8fb62c0d
Dorsa Maleki <a.malekidorsa@gmail.com>
2020-08-11 13:01:46 +0430

    initial dashboard changes

--- src/Home.js
+++ src/Home.js
@@ -4,32 +4,96 @@ import universityiconcolored from "./universityiconcolored.png";
 import { NavLink } from "react-router-dom";
 import codeiconcolored from "./codeiconcolored.png";
 import coffeeiconcolored from "./coffeeiconcolored.png";
+import { Getclass } from "./Getclass";
+
 export const Home = () => {
+  const classes = Getclass();
+
   return (
     <div className={styles.root}>
-      <div className={styles.box1}>
-        <img src={universityiconcolored} alt="university icon" />
-        <p>آخرین کلاس های من</p>
-        <NavLink to="/overview">
-          <div className={styles.text}>همه کلاس ها</div>
-        </NavLink>
-        <div className={styles.gridcontainer}>
-          <div>کلاس کامپیوتر دوره 34 </div>
+      <div className={styles.boxcontainer}>
+        <div className={styles.box1}>
+          <div className={styles.boxtop}>
+            <div>
+              <img src={universityiconcolored} alt="university icon" />
+              <div className={styles.text6}>آخرین کلاس های من</div>
+            </div>
+            <div className={styles.item2}>
+              <NavLink to="/overview">
+                <div className={styles.text}>همه کلاس ها</div>
+              </NavLink>
+            </div>
+          </div>
+          <div className={styles.box1bottom}>
+            {classes.map((co) => (
+              <div className={styles.gridcontainerbox1}>
+                <div>
+                  <div className={styles.text1}>کلاس {co.name}</div>
+                  <div className={styles.text2}>{co.professor}</div>
+                </div>
+                <div>
+                  <div className={styles.text2}>آخرین تمرین:</div>
+                  <div className={styles.text}>آزمون</div>
+                </div>
+                <div className={styles.text2}>
+                  <div>{co.members} عضو </div>
+                  <div>{co.date}</div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className={styles.box2}>
-        <img src={codeiconcolored} alt="code icon" />
-        <p>تمرین کنید</p>
-        <NavLink to="/problemset">
-          <div className={styles.text}>همه سوالات</div>
-        </NavLink>
-      </div>
-      <div className={styles.box3}>
-        <img src={coffeeiconcolored} alt="code icon" />
-        <p>مهارت خود را بسنجید</p>
-        <NavLink to="/contest">
-          <div className={styles.text}> همه مسابقات</div>
-        </NavLink>
+        <div className={styles.box2}>
+          <div className={styles.boxtop}>
+            <div>
+              <img src={codeiconcolored} alt="code icon" />
+              <div className={styles.text7}> تمرین کنید</div>
+            </div>
+            <div className={styles.item2}>
+              <NavLink to="/problemset">
+                <div className={styles.text}>همه سوالات</div>
+              </NavLink>
+            </div>
+          </div>
+          <div className={styles.box2bottom}>
+            <div className={styles.text2}>تعداد سوالات حل شده</div>
+            <div className={styles.gridcontainerbox2}>
+              <div className={styles.text3}>0</div>
+              <div className={styles.text4}>0</div>
+              <div className={styles.text5}>0</div>
+              <div className={styles.text3}>0</div>
+              <div className={styles.text1}>تکنولوژی</div>
+              <div className={styles.text1}>مسابقه</div>
+              <div className={styles.text1}>دانشگاهی</div>
+              <div className={styles.text1}>المپیاد</div>
+            </div>
+            <div className={styles.text2}>سؤالات پیشنهادی</div>
+          </div>
+        </div>
+        <div className={styles.box3}>
+          <div className={styles.boxtop}>
+            <div>
+              <img src={coffeeiconcolored} alt="code icon" />
+              <div className={styles.text8}>مهارت خود را بسنجید</div>
+            </div>
+            <div className={styles.item2}>
+              <NavLink to="/contest">
+                <div className={styles.text}> همه مسابقات</div>
+              </NavLink>
+            </div>
+          </div>
+          <div className={styles.box3bottom}>
+            <div className={styles.text2}>
+              <div>
+                در حال حاضر مسابقه فعالی وجود ندارد. می‌توانید سؤالات مسابقات
+                برگزارشده را در قسمت سؤالات حل کنید.
+              </div>
+              <div>
+                برای مطلع شدن از مسابقات جدید، به کانال تلگرام ما بپیوندید.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
