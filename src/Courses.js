7b65b3f1dd25b8fc89a65ae30cca57f8d23224a7
Dorsa Maleki <a.malekidorsa@gmail.com>
2020-08-12 20:41:32 +0430

    star icon

--- src/Courses.js
+++ src/Courses.js
@@ -11,48 +11,58 @@ export const Courses = () => {
   return (
     <div className={styles.root}>
       {console.log({ courses })}
-      <div className={styles.topclass1}>
-        <img
-          src={universityiconcolored}
-          alt="university icon"
-          className={styles.image}
-        />
-        <div className={styles.text1}>کلاس در یک نگاه</div>
-      </div>
-      <div className={styles.class1}>
-        {classes.map((co) => (
-          <div>
-            <div className={styles.name}>{co.name}</div>
-            <div className={styles.subject}>{co.professor}</div>
-            <br />
-            <div className={styles.line1}>
-              <span>دستیاران آموزشی</span>
-            </div>
-            <div className={styles.subject}>{co.TA}</div>
-
-            <div className={styles.line2}></div>
-            <div className={styles.gridContainer1}>
-              <div className={styles.member1}>{co.members}</div>
-              <div className={styles.member2}>4</div>
-              <div>تعداد اعضا</div>
-              <div>تعداد نوشته ها</div>
-            </div>
-            <br />
-            <div className={styles.gridContainer2}>
-              <div className={styles.number1}>16</div>
-              <div className={styles.number2}>19</div>
-              <div className={styles.number3}>11</div>
-              <div className={styles.text}>خوانده نشده</div>
-              <div className={styles.text}>بدون بحث</div>
-              <div className={styles.text}>حل نشده</div>
-            </div>
+      <div className={styles.gridContainer}>
+        <div>
+          <div className={styles.topclass1}>
+            <img
+              src={universityiconcolored}
+              alt="university icon"
+              className={styles.image}
+            />
+            <div className={styles.text1}>کلاس در یک نگاه</div>
           </div>
-        ))}
-      </div>
+          <div className={styles.class1}>
+            {classes.map((co) => (
+              <div>
+                <div className={styles.name}>{co.name}</div>
+                <div className={styles.subject}>{co.professor}</div>
+                <br />
+                <div className={styles.line1}>
+                  <span>دستیاران آموزشی</span>
+                </div>
+                <div className={styles.subject}>{co.TA}</div>
 
-      <div className={styles.box2}>
-        <img src={stariconorange} alt="star icon" className={styles.image} />
-        <div className={styles.text2}> نوشته های مهم</div>
+                <div className={styles.line2}></div>
+                <div className={styles.gridContainer1}>
+                  <div className={styles.member1}>{co.members}</div>
+                  <div className={styles.member2}>4</div>
+                  <div>تعداد اعضا</div>
+                  <div>تعداد نوشته ها</div>
+                </div>
+                <br />
+                <div className={styles.gridContainer2}>
+                  <div className={styles.number1}>16</div>
+                  <div className={styles.number2}>19</div>
+                  <div className={styles.number3}>11</div>
+                  <div className={styles.text}>خوانده نشده</div>
+                  <div className={styles.text}>بدون بحث</div>
+                  <div className={styles.text}>حل نشده</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          <div className={styles.topbox2}>
+            <img
+              src={stariconorange}
+              alt="star icon"
+              className={styles.image}
+            />
+            <div className={styles.text2}> نوشته های مهم</div>
+          </div>
+          <div className={styles.box2}></div>
+        </div>
       </div>
     </div>
   );
