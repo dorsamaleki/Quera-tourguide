49e593124091a43dcd3d13e24f7fed6b250d42eb
Dorsa Maleki <a.malekidorsa@gmail.com>
2020-08-18 11:28:44 +0430

    Updates

---
+++ precache-manifest.50eee98f3b2d8b76541c6add1d7c10bf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30762e71a99fb746a1cff7cd6a37254",
+    "url": "/Quera-tourguide/index.html"
+  },
+  {
+    "revision": "11433b2a4b23b7381e6b",
+    "url": "/Quera-tourguide/static/css/main.c01d2acc.chunk.css"
+  },
+  {
+    "revision": "22f073da62d6e18183d1",
+    "url": "/Quera-tourguide/static/js/2.21f0c83b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Quera-tourguide/static/js/2.21f0c83b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11433b2a4b23b7381e6b",
+    "url": "/Quera-tourguide/static/js/main.43f9f15b.chunk.js"
+  },
+  {
+    "revision": "23cf000dafddf04a3b20",
+    "url": "/Quera-tourguide/static/js/runtime-main.3c1619f7.js"
+  },
+  {
+    "revision": "0444ac73db92abb5962c2899fc2a56d3",
+    "url": "/Quera-tourguide/static/media/class1.0444ac73.jpg"
+  },
+  {
+    "revision": "42aada5a2ecdb536f3bb7c6dcd01d1ad",
+    "url": "/Quera-tourguide/static/media/codeicon.42aada5a.png"
+  },
+  {
+    "revision": "ffd46cf950fa5b35546767b2a05356d2",
+    "url": "/Quera-tourguide/static/media/stariconorange.ffd46cf9.png"
+  }
+]);
